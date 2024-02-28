ef51004254fa964b94958df729f91c4b34f4c088
mani <mani@email.com>
2024-02-28 08:41:16 +0530

    app routing fixed

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./navbar/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="main-container">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
